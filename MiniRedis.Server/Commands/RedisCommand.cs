0d86e1569d84d5c7dd395bda954fed77933b06b9
goktugibolar <mstfgktg2003@hotmail.com>
2026-08-24 01:23:31 +0300

    add redis command

---
+++ MiniRedis.Server/Commands/RedisCommand.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniRedis.Server.Commands;
+internal sealed record RedisCommand(string Name, IReadOnlyList<string> Args);
